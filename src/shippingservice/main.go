3c47e561b67b4a777dd52ac3157c3a8e238c2a44
haseeb <haseeb@cloudplex.io>
2019-08-16 13:29:22 +0500

    watch healthapi added

--- src/shippingservice/main.go
+++ src/shippingservice/main.go
@@ -16,6 +16,8 @@ package main
 
 import (
 	"fmt"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"net"
 	"os"
 	"time"
@@ -96,6 +98,9 @@ type server struct {
 // Check is for health checking.
 func (s *server) Check(ctx context.Context, req *healthpb.HealthCheckRequest) (*healthpb.HealthCheckResponse, error) {
 	return &healthpb.HealthCheckResponse{Status: healthpb.HealthCheckResponse_SERVING}, nil
+}
+func (s *server) Watch(req *healthpb.HealthCheckRequest, ws healthpb.Health_WatchServer) error {
+	return status.Errorf(codes.Unimplemented, "health check via Watch not implemented")
 }
 
 // GetQuote produces a shipping quote (cost) in USD.
